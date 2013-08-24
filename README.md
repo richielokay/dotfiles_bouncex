cc2cae97fd6c71674679d96640634f4e1a0e07cb
Richie Lokay <richie@richielokay.com>
2013-08-24 14:12:29 -0400

    Bouncex centric Richie customization

--- README.md
+++ README.md
@@ -49,14 +49,8 @@ If `~/.extra` exists, it will be sourced along with the other files. You can use
 My `~/.extra` looks something like this:
 
 ```bash
-# Git credentials
-# Not in the repository, to prevent people from accidentally committing under my name
-GIT_AUTHOR_NAME="Mathias Bynens"
-GIT_COMMITTER_NAME="$GIT_AUTHOR_NAME"
-git config --global user.name "$GIT_AUTHOR_NAME"
-GIT_AUTHOR_EMAIL="mathias@mailinator.com"
-GIT_COMMITTER_EMAIL="$GIT_AUTHOR_EMAIL"
-git config --global user.email "$GIT_AUTHOR_EMAIL"
+
+
 ```
 
 You could also use `~/.extra` to override settings, functions and aliases from my dotfiles repository. It’s probably better to [fork this repository](https://github.com/mathiasbynens/dotfiles/fork) instead, though.
---
+++ brew_bouncex.sh
@@ -0,0 +1,58 @@
+sudo gem install compass
+
+#!/usr/bin/env bash
+
+# Make sure we’re using the latest Homebrew
+brew update
+
+# Upgrade any already-installed formulae
+brew upgrade
+
+# Install Bash 4
+brew install bash
+
+# Install wget with IRI support
+brew install wget --enable-iri
+
+# Install more recent versions of some OS X tools
+brew tap homebrew/dupes
+brew install homebrew/dupes/grep
+brew tap josegonzalez/homebrew-php
+
+brew install php54-apc
+brew install imagemagick
+brew install php54-curl
+brew install php54-imagick
+
+brew install curl
+brew install mysql51
+
+# Install native apps
+brew tap phinze/homebrew-cask
+brew install brew-cask
+
+function installcask() {
+	brew cask install "${@}" 2> /dev/null
+}
+
+installcask github
+installcask gitx
+installcask gitbox
+installcask tower
+installcask iterm2
+installcask macvim
+installcask mysql-workbench
+installcask sequel-pro
+installcask sublime-text
+installcask the-unarchiver
+installcask transmission
+
+# Remove outdated versions from the cellar
+brew cleanup
+
+# Setup MYSQL to launch and restart
+
+echo "unset TMPDIR mysql_install_db"
+echo "ln -sfv /usr/local/opt/mysql51/*.plist ~/Library/LaunchAgents"
+echo "launchctl load ~/Library/  LaunchAgents/homebrew.mxcl.mysql51.plist"
+echo "mysql.server start"
